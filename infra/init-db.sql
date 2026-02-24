768cfed4c2150ab1fb6c67774dcfefb543729f21
Sindoval <sindoval321@gmail.com>
2026-02-24 20:16:50 -0300

    Ajustes docker-compose

--- infra/init-db.sql
+++ infra/init-db.sql
@@ -1,8 +1,2 @@
 CREATE DATABASE db_usuario;
 CREATE DATABASE db_comunicacao;
-
-\c db_usuario;
-
-INSERT INTO usuario (nome, email, senha)
-VALUES ('Admin', 'admin@admin.com', '123456')
-ON CONFLICT (email) DO NOTHING;
